8f1c2b35ea6983331cb3e8e79f0047e16b902ca0
ZeroParticle <jackwschaffner@gmail.com>
2022-06-06 13:59:46 -0700

    Remove some exports

--- src/init.lua
+++ src/init.lua
@@ -16,7 +16,6 @@ exports.ClientRemoteSignal = require(script.Knit.Util.Remote.ClientRemoteSignal)
 
 exports.Component = require(script.Knit.Util.Component)
 exports.Loader = require(script.Knit.Util.Loader)
-exports.Maid = require(script.Knit.Util.Maid)
 exports.Option = require(script.Knit.Util.Option)
 exports.Promise = require(script.Knit.Util.Promise)
 exports.Ser = require(script.Knit.Util.Ser)
@@ -25,6 +24,5 @@ exports.Streamable = require(script.Knit.Util.Streamable)
 exports.StreamableUtil = require(script.Knit.Util.StreamableUtil)
 exports.Symbol = require(script.Knit.Util.Symbol)
 exports.TableUtil = require(script.Knit.Util.TableUtil)
-exports.Thread = require(script.Knit.Util.Thread)
 
 return exports
